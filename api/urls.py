d8eaf429da66be7275eafeda88a0ea2c66be68c3
m1 <uzbzarif@gmail.com>
2024-03-28 15:53:55 +0500

    api_view

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 
 from django.urls import path
-from .views import Index
+from .views import Index, home
 urlpatterns = [
 path('', Index.as_view(), name='index'),
+path('home/', home, name='home'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from django.views import View
+
 from django.http import HttpResponse
+from rest_framework import request
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+
 # Create your views here.
 
 class Index(View):
     def get(self, request):
         return HttpResponse("Hello, world. You're at the polls index.")
+    
+
+@api_view(['GET'])
+def home(request: request):
+    print(request)
+    print(request.query_params)
+    data = {
+        "message": "Hello, world. You're at the polls index."
+    }
+    return Response(data,status=status.HTTP_200_OK)
+    
